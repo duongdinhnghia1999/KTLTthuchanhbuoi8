73acf048092450d31965c916ce9075acee4f975e
duongdinhnghia1999 <49018272+duongdinhnghia1999@users.noreply.github.com>
2019-06-04 15:07:20 +0700

    Add files via upload

--- 4.py
+++ 4.py
@@ -3,11 +3,11 @@ from tkinter import *
 wd = Tk()
 wd.title("Wellcome to LikeGeeks app")
 wd.geometry('350x200')
-lbl = Label(wd, text = "Hello")
+lbl = Label(wd, text = "Hello", fg= "red", background= "yellow")
 lbl.grid(column = 0, row = 0)
 
 def clicked():
-    lbl.configure(text = "Button was clicked!")
+    lbl.configure(text = "Button was clicked!", fg= "blue", background= "violet")
 btn = Button(wd, text = "Click Me", command = clicked)
 btn.grid(column = 1, row = 0)
 wd.mainloop()
--- 7.py
+++ 7.py
@@ -4,7 +4,7 @@ import random
 colours = ['Red', 'Blue', 'Green', 'Pink', 'Black', 'Yellow', 'Orange', 'White', 'Purple', 'Brown']
 score = 0
 
-timeleft = 30
+timeleft = 120
 
 def startGame(event):
     if timeleft == 30:
@@ -17,7 +17,7 @@ def nextColour():
     if timeleft > 0:
         e.focus_set()
         if e.get().lower() == colours[1].lower():
-            score += 1
+            score += 2
         e.delete(0, tkinter.END)
         random.shuffle(colours)
 
@@ -27,7 +27,7 @@ def nextColour():
 def countdown():
     global timeleft
     if timeleft > 0:
-        timeleft -= 1
+        timeleft -= -1
         timelabel.config(text = "Time left: " + str(timeleft))
         timelabel.after(1000, countdown)
 
@@ -35,16 +35,16 @@ root = tkinter.Tk()
 root.title("COLORGAME")
 root.geometry("375x200")
 
-instructions = tkinter.Label(root, text = "Type in the colour of the words, and not the word text!", font = ('Times', 12))
+instructions = tkinter.Label(root, text = "Type in the colour of the words, and not the word text!", font = ('Times New Roman', 14))
 instructions.pack()
 
-scoreLabel = tkinter.Label(root, text = "Press enter to start", font = ('Times', 12))
+scoreLabel = tkinter.Label(root, text = "Press enter to start", font = ('Times New Roman', 14))
 scoreLabel.pack()
 
-timeLabel = tkinter.Label(root, text = "Time left: " + str(timeleft), font = ('Times', 12))
+timeLabel = tkinter.Label(root, text = "Time left: " + str(timeleft), font = ('Times New Roman', 14))
 timeLabel.pack()
 
-label = tkinter.Label(root, font = ('Times', 12))
+label = tkinter.Label(root, font = ('Times New Roman', 14))
 label.pack()
 
 e = tkinter.Entry(root)
